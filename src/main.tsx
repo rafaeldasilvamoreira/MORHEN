aafca0b1cd06bb1ffd21e78b31bbbdb2f6104662
Rafael Moreira <moreirarafinha30@gmail.com>
2025-09-15 12:16:11 -0300

    alterado endereço dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
-import { Dashboard } from './root/Dashboard'
+import { Dashboard } from './root/dashboard'
 import { ServicesPage } from './root/servicesPage'
 import { AboutPage } from './root/about'
 import { ContactPage } from './root/contact'
